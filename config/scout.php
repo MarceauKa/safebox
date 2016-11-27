724be53fc3079c141cf698a8280b41a334c075f0
MarceauKa <marceau@casals.fr>
2016-11-27 17:24:58 +0100

    Enable search fuzzyness by default

--- config/scout.php
+++ config/scout.php
@@ -87,7 +87,7 @@ return [
     */
     'tntsearch' => [
         'storage'  => storage_path('tnt'), //place where the index files will be stored
-        'fuzziness' => env('TNTSEARCH_FUZZINESS', false),
+        'fuzziness' => env('TNTSEARCH_FUZZINESS', true),
         'fuzzy' => [
             'prefix_length' => 2,
             'max_expansions' => 50,
